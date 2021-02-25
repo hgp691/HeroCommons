80a36bb081f57080912ee1c052914a468c775b9a
Horacio Guzmán Parra <hgp691@gmail.com>
2021-02-25 15:31:09 -0500

    build XCFramework

---
+++ Carthage/Build/HeroCommons.xcframework/ios-arm64/HeroCommons.framework/Headers/HeroCommons.h
@@ -0,0 +1,18 @@
+//
+//  HeroCommons.h
+//  HeroCommons
+//
+//  Created by Horacio Guzman on 18/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HeroCommons.
+FOUNDATION_EXPORT double HeroCommonsVersionNumber;
+
+//! Project version string for HeroCommons.
+FOUNDATION_EXPORT const unsigned char HeroCommonsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HeroCommons/PublicHeader.h>
+
+
